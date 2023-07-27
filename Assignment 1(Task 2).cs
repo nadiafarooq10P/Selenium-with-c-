bbcf30bfa15e830a20c5c49b912d950dd30188de
nadia.farooq <nadia.farooq@10pearls.com>
2023-07-27 12:43:45 +0500

    commiting my 1st assignment

---
+++ Assignment 1(Task 2).cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Reflection;
+using static System.Net.WebRequestMethods;
+using System;
+using System.Threading;
+
+namespace SeleniumBatch3
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void Assignment2()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://10pearlsuniversity.org/");
+            Console.WriteLine(driver.Title);
+            string s = driver.Title.ToString();
+            if (s == "Home - 10PEARLS University")
+            {
+
+                Console.WriteLine("Title of the Page: Home - 10PEARLS University");
+            }
+
+            else
+            {
+                Console.WriteLine("Failed");
+
+            }
+
+            driver.Navigate().GoToUrl("https://saucedemo.com/");
+            driver.Navigate().Back();
+            Console.WriteLine(driver.Url);
+            driver.Navigate().Refresh();
+            driver.Close();
+
+        }
+    }
+}
+
---
+++ Assignment1(Task1).cs
@@ -0,0 +1,46 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Reflection;
+using static System.Net.WebRequestMethods;
+
+namespace SeleniumBatch3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Assignment1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");
+            Assert.IsTrue(driver.Url.Equals("https://practicetestautomation.com/practice-test-login/"));
+
+            //Assert.AreEqual(driver.Url.Contains("https://10pearlsuniversity.org/"), "Passed")
+
+            driver.FindElement(By.CssSelector("input[name='username']")).SendKeys("student");
+            driver.FindElement(By.CssSelector("input[name='password']")).SendKeys("Password123");
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//*[@id=\"submit\"]")).Click();
+            // Assert.AreEqual(driver.Url.Contains("https://practicetestautomation.com/logged-in-successfully/"), "https://practicetestautomation.com/logged-in-successfully/");
+            Thread.Sleep(2000);
+
+            if (driver.Url.Contains("https://practicetestautomation.com/logged-in-successfully/"))
+            {
+                Console.WriteLine("Login successfully");
+            }
+
+            else
+            {
+                Console.WriteLine("Failed");
+
+            }
+            driver.Close();
+
+
+        }
+        
+    }
+}
+    
