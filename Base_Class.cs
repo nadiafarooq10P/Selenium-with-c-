0365a672cc91694cdc666334105f3d8284bf7473
nadiafarooq10P <115792592+nadiafarooq10P@users.noreply.github.com>
2023-08-02 12:40:04 +0500

    Add files via upload

---
+++ Base_Class.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sel_Training
+{
+    public class Base_Class
+    {
+        public IWebDriver driver = new ChromeDriver();
+
+        // Initialize Driver
+        [TestInitialize]
+        public void WebdriverInitialization1()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        //Closing Driver
+        [TestCleanup]
+        public void ClosingBrowser2()
+        {
+            driver.Close();
+        }
+
+    }
+}
